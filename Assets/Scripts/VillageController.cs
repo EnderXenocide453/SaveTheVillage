d4365e575e17596295811a8bd2b7dbf93d33272b
EnderXenocide453 <lakssterius@gmail.com>
2023-10-07 13:20:08 +0300

    Добавлен найм воинов

--- Assets/Scripts/VillageController.cs
+++ Assets/Scripts/VillageController.cs
@@ -40,17 +40,15 @@ public class VillageController : MonoBehaviour
     [SerializeField]
     private Soldier archer;
 
-    private int warriorCount;
-    private int defenderCount;
-    private int archerCount;
-
-    private int people;
+    private int _people;
+    private bool _updateSoldierButtons = true;
     #endregion
 
     private void Start()
     {
         InitResources();
         InitTimers();
+        InitSoldiers();
     }
 
     public void StartAddEmployee(ResourceSource src)
@@ -65,7 +63,7 @@ public class VillageController : MonoBehaviour
 
     public void RemoveEmployee(ResourceSource src)
     {
-        people += src.RemoveEmployee();
+        _people += src.RemoveEmployee();
     }
 
     /// <summary>
@@ -74,8 +72,13 @@ public class VillageController : MonoBehaviour
     private void InitResources()
     {
         food.Init();
+        food.onAmountChanged += CalculateSoldierButtons;
+
         wood.Init();
+        wood.onAmountChanged += CalculateSoldierButtons;
+
         metal.Init();
+        metal.onAmountChanged += CalculateSoldierButtons;
     }
 
     private void InitTimers()
@@ -92,7 +95,7 @@ public class VillageController : MonoBehaviour
         turn++;
         turnField.text = turn.ToString();
 
-        if (food.RemoveResource(people) > 0)
+        if (food.RemoveResource(_people) > 0)
             Hunger();
     }
 
@@ -101,9 +104,93 @@ public class VillageController : MonoBehaviour
         Debug.Log("Начался голод");
     }
 
+    private void InitSoldiers()
+    {
+        CalculateSoldierButtons();
+
+        //Кнопки добавления
+        warrior.plusBtn.onClick.AddListener(delegate () {
+            StartAddsoldier(warrior);
+        });
+        defender.plusBtn.onClick.AddListener(delegate () {
+            StartAddsoldier(defender);
+        });
+        archer.plusBtn.onClick.AddListener(delegate () {
+            StartAddsoldier(archer);
+        });
+        //кнопки удаления
+        warrior.minusBtn.onClick.AddListener(delegate () {
+            RemoveSoldier(warrior);
+        });
+        defender.minusBtn.onClick.AddListener(delegate () {
+            RemoveSoldier(defender);
+        });
+        archer.minusBtn.onClick.AddListener(delegate () {
+            RemoveSoldier(archer);
+        });
+    }
+
+    private void RemoveSoldier(Soldier soldier)
+    {
+        _people += soldier.RemoveSoldiers(1);
+    }
+
+    private void StartAddsoldier(Soldier soldier)
+    {
+        DisableSoldierButtons();
+
+        soldierTimer.SetTime(soldier.hireTime);
+        soldierTimer.ClearListeners();
+        soldierTimer.onLoopEnds += () => AddSoldier(soldier);
+        soldierTimer.Play();
+    }
+
+    private void DisableSoldierButtons()
+    {
+        _updateSoldierButtons = false;
+
+        warrior.plusBtn.interactable = false;
+        defender.plusBtn.interactable = false;
+        archer.plusBtn.interactable = false;
+    }
+
+    private void AddSoldier(Soldier soldier)
+    {
+        soldierTimer.Stop();
+        _people += soldier.AddSoldiers(1);
+
+        _updateSoldierButtons = true;
+        CalculateSoldierButtons();
+    }
+
+    private void CalculateSoldierButtons()
+    {
+        if (!_updateSoldierButtons) return;
+
+        CalculateSoldierButton(warrior);
+        CalculateSoldierButton(defender);
+        CalculateSoldierButton(archer);
+    }
+
+    private void CalculateSoldierButton(Soldier soldier)
+    {
+        if (soldier.foodCost > food.curAmount) {
+            soldier.plusBtn.interactable = false;
+            return;
+        } if (soldier.woodCost > wood.curAmount) {
+            soldier.plusBtn.interactable = false;
+            return;
+        } if (soldier.metalCost > metal.curAmount) {
+            soldier.plusBtn.interactable = false;
+            return;
+        }
+
+        soldier.plusBtn.interactable = true;
+    }
+
     private void AddEmployee(ResourceSource src)
     {
-        people += src.AddEmployee();
+        _people += src.AddEmployee();
         employeeTimer.Stop();
 
         SetEmployeesButtonsActive(true);
@@ -172,14 +259,52 @@ public class Resource
 [System.Serializable]
 public class Soldier
 {
+    #region Стоимость найма и время на найм
     public int foodCost;
     public int woodCost;
     public int metalCost;
 
     public float hireTime = 15;
+    #endregion
 
+    #region Боевые параметры
     public int attack;
     public int defense;
 
     public float attackPriority = 1;
+    #endregion
+
+    public int curAmount { get; private set; }
+
+    #region UI
+    public Button plusBtn, minusBtn;
+    public Text countfield;
+    #endregion
+
+    public int AddSoldiers(int amount = 1)
+    {
+        return SetSoldiers(curAmount + amount);
+    }
+
+    public int RemoveSoldiers(int amount = 1)
+    {
+        return SetSoldiers(curAmount - amount);
+    }
+
+    private int SetSoldiers(int amount)
+    {
+        amount = Mathf.Clamp(amount, 0, int.MaxValue);
+        //Разница между новым и старым значениями количества воинов
+        int delta = amount - curAmount;
+        curAmount = amount;
+
+        UpdateUI();
+
+        return delta;
+    }
+
+    private void UpdateUI()
+    {
+        countfield.text = curAmount.ToString();
+    }
 }
